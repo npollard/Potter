2fe1ff8b6389787cf5502ebef234503eb4380db5
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-02-24 19:58:17 -0800

    Simple discounts work

--- src/main/java/com/nelson/app/Potter.java
+++ src/main/java/com/nelson/app/Potter.java
@@ -1,9 +1,55 @@
 package com.nelson.app;
 
+
 public class Potter
 {
   public double getPrice(int[] books) {
-    return books.length * 8;
+    if (books.length == 0) {
+      return 0;
+    }
+
+    if (books.length == 1) {
+      return 8;
+    }
+
+    int uniqueTitles = 1;
+    int prevTitle = books[0];
+
+    System.out.printf("books[0]: %d\n", books[0]); //DEBUG
+    
+    for (int i = 1; i < books.length; i++) {
+      if (books[i] != prevTitle) uniqueTitles++;
+
+      System.out.printf("books[%d]: %d\n", i, books[i]); //DEBUG
+    }
+    
+    System.out.printf("UNIQUE TITLES = %d\n", uniqueTitles); //DEBUG
+
+    double price = getDiscountRate(uniqueTitles) + (books.length - uniqueTitles) * 8;
+    
+    return price;
+  }
+
+
+  private double getDiscountRate(int uniqueTitles) {
+    double discountRate = 1;
+    switch (uniqueTitles) {
+      case 1: discountRate = 1;
+              break;
+      case 2: discountRate = .95;
+              break;
+      case 3: discountRate = .9;
+              break;
+      case 4: discountRate = .8;
+              break;
+      case 5: discountRate = .75;
+              break;
+      default: break;
+
+    }
+
+    return discountRate * uniqueTitles * 8;
+        
   }
 
 }
--- src/test/java/com/nelson/app/PotterTest.java
+++ src/test/java/com/nelson/app/PotterTest.java
@@ -10,14 +10,21 @@ import org.junit.Test;
 public class PotterTest {
   Potter p = new Potter();
   int[][] books = {
-    {},
-    {0},
-    {1},
-    {2},
-    {3},
-    {4},
-    {0, 0},
-    {1, 1, 1}
+    {}, //0
+    {0}, //1
+    {1}, //2
+    {2}, //3
+    {3}, //4
+    {4}, //5
+    {0, 0}, //6
+    {1, 1, 1}, //7
+    {0, 1}, //8
+    {0, 2, 4}, //9
+    {0, 1, 2, 4}, //10
+    {0, 1, 2, 3, 4}, //11
+    {0, 0, 1}, //12
+    {0, 0, 1, 1} 
+
   };
 
 
@@ -31,9 +38,23 @@ public class PotterTest {
     assertEquals(8, p.getPrice(books[5]), .0001);
     assertEquals(2 * 8, p.getPrice(books[6]), .0001);
     assertEquals(3 * 8, p.getPrice(books[7]), .0001);
-
-
   
+  }
+
+  @Test
+  public void testSimpleDiscounts() {
+    assertEquals(8 * 2 * .95, p.getPrice(books[8]), .0001);
+    assertEquals(8 * 3 * .9, p.getPrice(books[9]), .0001);
+    assertEquals(8 * 4 * .8, p.getPrice(books[10]), .0001);
+    assertEquals(8 * 5 * .75, p.getPrice(books[11]), .0001);
+
+  }
+
+  @Test
+  public void testMultipleDiscounts() {
+    assertEquals(8 + (8 * 2 * .95), p.getPrice(books[12]), .0001);
+    assertEquals(2 * (8 * 2 * .95), p.getPrice(books[13]), .0001);
+
   }
 
 
