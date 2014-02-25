7baf95dfbfd4f2e73d95255f91b1539b0023215c
nelson pollard <nelson.pollard@sv.cmu.edu>
2014-02-24 16:20:53 -0800

    Init potter repo

---
+++ src/main/java/com/nelson/app/Potter.java
@@ -0,0 +1,7 @@
+package com.nelson.app;
+
+public class Potter
+{
+
+
+}
---
+++ src/test/java/com/nelson/app/PotterTest.java
@@ -0,0 +1,27 @@
+package com.nelson.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+
+public class PotterTest {
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  
+  }
+
+	@Test
+	public void testTheTruth() {
+		assertTrue(true);
+	}
+
+ 
+}
